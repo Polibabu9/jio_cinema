8c9776ff86e5d9d9a25f31e8f1303f900ef824f6
Polibabu9 <addalapoli1@gmail.com>
2025-08-31 12:26:53 +0530

    no changes

--- api/index.js
+++ api/index.js
@@ -5,7 +5,8 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors());
-const port = 3000;
+
+const port = process.env.PORT || 3000;
 
 // Path to the JSON file
 const moviesFilePath = path.join(__dirname, 'movies_data.json');
@@ -24,7 +25,7 @@ function readMoviesData() {
 // Route to get all movies
 app.get('/movies', (req, res) => {
   const movies = readMoviesData();
-  res.send(movies);
+  res.json(movies); // safer for APIs
 });
 
 // Route to get a specific movie by ID
@@ -34,9 +35,9 @@ app.get('/movies/:id', (req, res) => {
   const movie = movies.find(m => m.id === movieId);
 
   if (movie) {
-    res.send(movie);
+    res.json(movie);
   } else {
-    res.status(404).send({ error: 'Movie not found' });
+    res.status(404).json({ error: 'Movie not found' });
   }
 });
 
